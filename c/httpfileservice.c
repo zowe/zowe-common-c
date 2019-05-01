3318e6b5b3f3b09df3dcf0f461436733492cda9a
Jordan Filteau <jfilteau@rocketsoftware.com>
2019-05-01 16:28:30 -0400

    Now following chunked protocol for HTTP when requesting downloads.
    Signed-off-by: Jordan Filteau <jfilteau@rocketsoftware.com>

--- c/httpfileservice.c
+++ c/httpfileservice.c
@@ -32,6 +32,7 @@
 #include "charsets.h"
 #include "unixfile.h"
 #include "httpfileservice.h"
+#include "xlate.h"
 
 #ifdef __ZOWE_OS_ZOS
 #define NATIVE_CODEPAGE CCSID_EBCDIC_1047
@@ -586,6 +587,237 @@ void respondWithUnixFileMetadata(HttpResponse *response, char *absolutePath) {
   }
 }
 
+int streamFileToCaller(HttpResponse *response, char *absolutePath) {
+  FileInfo info = {0};
+  int returnCode = 0;
+  int reasonCode = 0;
+  int status = 0;
+  
+  status = fileInfo(absolutePath, &info, &returnCode, &reasonCode);
+  if (status == -1) {
+    respondWithJsonError(response, "File Not Found", 404, "File Not Found");
+    return 0;
+  }
+  
+  UnixFile *requestedFile = fileOpen(absolutePath,
+                                     FILE_OPTION_READ_ONLY,
+                                     0,
+                                     0,
+                                     &returnCode,
+                                     &reasonCode);
+  
+  if (requestedFile == NULL) {
+    respondWithJsonError(response, "Forbidden", 403, "Forbidden");
+    return 0;
+  }
+  
+  setResponseStatus(response, 200, "OK");
+  addStringHeader(response, "Server", "jdmfws");
+  addStringHeader(response, "Transfer-Encoding", "chunked");
+  addStringHeader(response, "Content-Disposition", "attachment");
+  addStringHeader(response, "Connection", "close");
+	writeHeader(response);
+
+	short ccsid = info.ccsid;
+
+  /* Default to sending binary if we don't know. The only case,
+   * this would be bad is if the file is stored as IBM-1047.
+   * We are doing work on USS, but the uses of this API will
+   * most likely be used to get files to and from systems
+   * without EBCDIC. 
+   */
+  if (ccsid == CCSID_BINARY || ccsid == CCSID_UNTAGGED) {
+    zowelog(NULL, LOG_COMP_HTTPSERVER, ZOWE_LOG_DEBUG2,
+            "Sending file as binary...\n");
+            
+    addStringHeader(response, "Content-Type", "application/octet-stream");
+    writeHeader(response);
+      
+    sendBinaryDataToCaller(requestedFile, response->socket);
+  } 
+  else {
+    zowelog(NULL, LOG_COMP_HTTPSERVER, ZOWE_LOG_DEBUG2,
+            "Sending file as text...\n");
+            
+    addStringHeader(response, "Content-Type", "text/plain");
+    writeHeader(response);
+            
+    sendTextDataToCaller(requestedFile, response->socket, ccsid,
+                         CCSID_UTF_8);
+  }
+  
+  fileClose(requestedFile, &returnCode, &reasonCode);
+  
+  finishResponse(response);
+  
+  return 0;
+}
+
+#define HTTP_CHUNKED_SIZE 4096
+
+/* Returns a string with a hexadecimal number, followed by \r\n, 
+ * and a '\0'.
+ *
+ * This string IS null terminated.
+ */
+static int formatLengthChunk(char *chunkBuffer, int chunkBufferSize, int chunkLength) {
+  const void *table = getTranslationTable("ibm1047_to_iso88591");
+  
+  snprintf(chunkBuffer, chunkBufferSize, "%x\r\n", chunkLength);
+  translate(chunkBuffer, table, chunkBufferSize); /* EBCDIC to ASCII */
+}
+
+/* Returns a string with the file data, followed by \r\n.
+ *
+ * This string is NOT null terminated.
+ */
+static int formatDataChunk(char *chunkBuffer, int dataLength, char *dataBuffer) {
+  memcpy(chunkBuffer, dataBuffer, dataLength);
+  chunkBuffer[dataLength] = 0x0d;
+  chunkBuffer[dataLength+1] = 0x0a;
+}
+
+/* Sends the last chunk to the client. It is a chunk with
+ * a size of 0, followed by \r\n\r\n.
+ */
+static int sendLastChunk(Socket *socket) {
+  char lastChunkSizeBuffer[] = {0x30, 0x0d, 0x0a};
+  writeFully(socket, lastChunkSizeBuffer, sizeof(lastChunkSizeBuffer));
+      
+  char lastChunk[] = {0x0d, 0x0a};
+  writeFully(socket, lastChunk, sizeof(lastChunk));
+}
+
+static int sendBinaryDataToCaller(UnixFile *file, Socket *socket) { 
+  while (1) {
+    int returnCode = 0;
+    int reasonCode = 0;
+    char fileBuffer[HTTP_CHUNKED_SIZE] = {0};
+    
+    int bytesRead = fileRead(file, fileBuffer, HTTP_CHUNKED_SIZE, 
+                             &returnCode, &reasonCode);
+    
+    if (bytesRead <= 0) {
+      if (returnCode == -1) {
+        zowelog(NULL, LOG_COMP_HTTPSERVER, ZOWE_LOG_DEBUG2,
+               "Error reading file, rsn = %0x\n", reasonCode);
+      }
+
+      break; /* Send last chunk to signal end */
+    }
+
+    char chunkSizeBuffer[16 + 2 + 1] = {0}; /* +2 for crlf, +1 for '\0' */
+		formatLengthChunk(chunkSizeBuffer, sizeof(chunkSizeBuffer), bytesRead);
+        
+    int success = writeFully(socket, chunkSizeBuffer, strlen(chunkSizeBuffer));
+    if (!success) {
+      zowelog(NULL, LOG_COMP_HTTPSERVER, ZOWE_LOG_DEBUG2,
+              "Failed to write length chunk to socket...\n");
+              
+      return -1; /* Last chunk can't be sent */
+    }
+    
+		char chunkData[bytesRead + 2]; /* +2 for crlf */
+    formatDataChunk(chunkData, bytesRead, fileBuffer);
+      
+    success = writeFully(socket, chunkData, sizeof(chunkData));
+    if (!success) {
+      zowelog(NULL, LOG_COMP_HTTPSERVER, ZOWE_LOG_DEBUG2,
+              "Failed to write data chunk to socket...\n");
+              
+      return -1; /* Last chunk can't be sent */
+    }
+  }  
+  
+  zowelog(NULL, LOG_COMP_HTTPSERVER, ZOWE_LOG_DEBUG2,
+         "Sending last chunk...\n");
+         
+  sendLastChunk(socket);
+  
+  return 0;
+}
+
+static int sendTextDataToCaller(UnixFile *file, Socket *socket, int sourceTag,
+                                int targetTag) {
+  while (1) {
+    int returnCode = 0;
+    int reasonCode = 0;
+    char fileBuffer[HTTP_CHUNKED_SIZE] = {0};
+    
+    int bytesRead = fileRead(file, fileBuffer, HTTP_CHUNKED_SIZE, 
+                             &returnCode, &reasonCode);
+    
+    if (bytesRead <= 0) {
+      if (returnCode == -1) {
+        zowelog(NULL, LOG_COMP_HTTPSERVER, ZOWE_LOG_DEBUG2,
+               "Error reading file, rsn = %0x\n", reasonCode);
+      }
+
+      break; /* Send last chunk to signal end */
+    }
+    
+    char translation[HTTP_CHUNKED_SIZE * 4] = {0}; /* UTF-8 can use 1-4 bytes per character. */
+    char *inPtr = fileBuffer;
+    char *outPtr = translation;
+    int inLen = bytesRead;
+    int outLen = bytesRead * 4; /* see above */
+    int translationLength = 0;
+    
+    if (sourceTag != targetTag) {
+      int status = convertCharset(inPtr,
+                                  inLen,
+                                  sourceTag,
+                                  CHARSET_OUTPUT_USE_BUFFER,
+                                  &outPtr,
+                                  outLen,
+                                  targetTag,
+                                  NULL,
+                                  &translationLength,
+                                  &reasonCode);
+                                
+      if (status != 0) {
+        zowelog(NULL, LOG_COMP_HTTPSERVER, ZOWE_LOG_DEBUG2,
+                "Conversion failed...\n");
+                
+        break; /* Send last chunk to signal end */
+      }
+    }
+    else {
+      translationLength = bytesRead;
+      memcpy(translation, fileBuffer, bytesRead);
+    }
+
+    
+    char chunkSizeBuffer[16 + 2 + 1] = {0}; /* +2 for crlf, +1 for '\0' */
+		formatLengthChunk(chunkSizeBuffer, sizeof(chunkSizeBuffer), translationLength);
+   
+    int success = writeFully(socket, chunkSizeBuffer, strlen(chunkSizeBuffer));
+    if (!success) {
+      zowelog(NULL, LOG_COMP_HTTPSERVER, ZOWE_LOG_DEBUG2,
+              "Failed to write length chunk to socket...\n");
+              
+      return -1; /* Last chunk can't be sent */
+    }
+    
+    char chunkData[translationLength + 2]; /* +2 for crlf */
+    formatDataChunk(chunkData, translationLength, translation);
+    
+    success = writeFully(socket, chunkData, sizeof(chunkData));
+    if (!success) {
+      zowelog(NULL, LOG_COMP_HTTPSERVER, ZOWE_LOG_DEBUG2,
+              "Failed to write data chunk to socket...\n");
+              
+      return -1; /* Last chunk can't be sent */
+    }
+  }  
+  
+  zowelog(NULL, LOG_COMP_HTTPSERVER, ZOWE_LOG_DEBUG2,
+         "Sending last chunk...\n");
+         
+  sendLastChunk(socket);
+  
+  return 0;
+}
 
 /*
   This program and the accompanying materials are
