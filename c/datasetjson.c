ddda754fcdac6aef5c1caa1191700a2111e2e3dd
Timothy Gerstel <tim.gerstel@gmail.com>
2020-01-15 16:32:00 -0500

    Add length checks so that asterisks are not appended to dataset names that are too long
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- c/datasetjson.c
+++ c/datasetjson.c
@@ -1944,7 +1944,7 @@ void respondWithDatasetMetadata(HttpResponse *response) {
 #ifdef __ZOWE_OS_ZOS
   HttpRequest *request = response->request;
   char *datasetOrMember = stringListPrint(request->parsedFile, 2, 2, "?", 0); /*get search term*/
-
+  
   if (datasetOrMember == NULL || strlen(datasetOrMember) < 1){
     respondWithError(response,HTTP_STATUS_BAD_REQUEST,"No dataset name given");
     return;
@@ -2071,35 +2071,38 @@ void respondWithDatasetMetadata(HttpResponse *response) {
       respondWithError(response, HTTP_STATUS_BAD_REQUEST,"Malformed resume dataset name");
     }
   }
+  
+  if(addQualifiersArg != NULL) {
+    int addQualifiers = !strcmp(addQualifiersArg, "true");
+#define DSN_MAX_LEN 44
+    char dsnNameNullTerm[DSN_MAX_LEN + 1] = {0}; //+1 for null term
+    memcpy(dsnNameNullTerm, dsnName.value, sizeof(dsnName.value));
+    nullTerminate(dsnNameNullTerm, sizeof(dsnNameNullTerm) - 1);
+    printf("dsnName.value: %s\n", dsnName.value);
+    if (addQualifiers && dsnLen <= DSN_MAX_LEN) {
+      int asteriskPos = lastIndexOf(dsnNameNullTerm, dsnLen,'*');
+      int dblAsteriskPos = indexOfString(dsnNameNullTerm, dsnLen, "**", 0); //"**" is only valid at the end of a query, cannot appear in the middle of a search
+      int periodPos = lastIndexOf(dsnNameNullTerm, dsnLen, '.');
+      if (asteriskPos < 0 && dblAsteriskPos < 0) {
+        if(dsnLen <= DSN_MAX_LEN - 3 && (periodPos < 0 || periodPos != dsnLen - 1)){ //dsnLen - 1 for null terminator.  Query in form of hlq1.hlq2
+          snprintf(dsnNameNullTerm, DSN_MAX_LEN + 1, "%s.**", dsnNameNullTerm);
+        }
+      } else {
+        if (dsnLen <= DSN_MAX_LEN - 3 && asteriskPos == dsnLen - 1 && periodPos != asteriskPos - 1 && dblAsteriskPos < 0) { //query in form of hlq1.hlq2*
+          snprintf(dsnNameNullTerm, DSN_MAX_LEN + 1, "%s.**", dsnNameNullTerm);
+        } else if(dsnLen <= DSN_MAX_LEN - 2 && asteriskPos == dsnLen - 1 && periodPos == asteriskPos - 1) {
+          snprintf(dsnNameNullTerm, DSN_MAX_LEN + 1, "%s*", dsnNameNullTerm);
+        }
+      }
+    }
+    memcpy(dsnName.value, dsnNameNullTerm, strlen(dsnNameNullTerm));
+    printf("new dsnName.value: %s\n", dsnName.value);
+#undef DSN_MAX_LEN
+  }
 
   int fieldCount = defaultCSIFieldCount;
   char **csiFields = defaultCSIFields;
-  int addQualifiers = !strcmp(addQualifiersArg, "true");
-#define DSN_MAX_LEN 44
-  char dsnNameNullTerm[DSN_MAX_LEN] = {0};
-  memcpy(dsnNameNullTerm, dsnName.value, sizeof(dsnName.value));
-  nullTerminate(dsnNameNullTerm, sizeof(dsnNameNullTerm) - 1);
-  if (addQualifiers && dsnLen < DSN_MAX_LEN) {
-    int asteriskPos = lastIndexOf(dsnNameNullTerm, dsnLen,'*');
-    int dblAsteriskPos = indexOfString(dsnNameNullTerm, dsnLen, "**", 0); //"**" is only valid at the end of a query, cannot appear in the middle of a search
-    int periodPos = lastIndexOf(dsnNameNullTerm, dsnLen, '.');
-    char newDsn[DSN_MAX_LEN];
-    if (asteriskPos < 0 && dblAsteriskPos < 0) {
-      if(periodPos < 0 || periodPos != dsnLen - 1){ //-1 for null terminator.  Query in form of hlq1.hlq2
-        snprintf(dsnNameNullTerm, DSN_MAX_LEN + 1, "%s.**", dsnNameNullTerm);
-      }else if(periodPos == dsnLen - 1){ //not sure if this case is ever valid, trailing periods seem to be truncated
-        snprintf(dsnNameNullTerm, DSN_MAX_LEN + 1, "%s**", dsnNameNullTerm);
-      }
-    } else {
-      if (asteriskPos == dsnLen - 1 && periodPos != asteriskPos - 1 && dblAsteriskPos < 0) { //query in form of hlq1.hlq2*
-        snprintf(dsnNameNullTerm, DSN_MAX_LEN + 1, "%s.**", dsnNameNullTerm);
-      } else if(asteriskPos == dsnLen - 1 && periodPos == asteriskPos - 1) {
-        snprintf(dsnNameNullTerm, DSN_MAX_LEN + 1, "%s*", dsnNameNullTerm);
-      }
-    }
-  }
-  memcpy(dsnName.value, dsnNameNullTerm, strlen(dsnNameNullTerm));
-#undef DSN_MAX_LEN
+  
   csi_parmblock *returnParms = (csi_parmblock*)safeMalloc(sizeof(csi_parmblock),"CSI ParmBlock");
   EntryDataSet *entrySet = returnEntries(dsnName.value, typesArg,datasetTypeCount, workAreaSizeArg, csiFields, fieldCount, resumeNameArg, resumeCatalogNameArg, returnParms); 
   char *resumeName = returnParms->resume_name;
