ceef3c5231f22fde96ad25ba6978ad781b7ec9f6
Suneeth Keerthy <skeerthy@rocketsoftware.com>
2020-01-27 15:48:12 -0500

    Add removed line to test build
    Signed-off-by: Suneeth Keerthy <skeerthy@rocketsoftware.com>

--- h/logging.h
+++ h/logging.h
@@ -342,6 +342,7 @@ void printStderr(LoggingContext *context, LoggingComponent *component, void *dat
 #endif
 
 
+
 /*
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
