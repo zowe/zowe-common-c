44e34331ead9f68fe0b73b1d504223f45e69dcdf
Irek Fakhrutdinov <ifakhrutdinov@rocketsoftware.com>
2020-02-14 21:23:11 +0100

    Better naming.
    Signed-off-by: Irek Fakhrutdinov <ifakhrutdinov@rocketsoftware.com>

--- c/recovery.c
+++ c/recovery.c
@@ -1060,7 +1060,7 @@ static CPID makeRecoveryStatePool(unsigned int primaryCellCount,
   return poolID;
 }
 
-static void removeStatePool(CPID pool) {
+static void removeRecoveryStatePool(CPID pool) {
   cellpoolDelete(pool);
 }
 
@@ -1216,7 +1216,7 @@ static int establishRouterInternal(RecoveryContext *userContext,
 #ifdef RCVR_CPOOL_STATES
       if (userStatePool == NULL &&
           context->stateCellPool != CPID_NULL) {
-        removeStatePool(context->stateCellPool);
+        removeRecoveryStatePool(context->stateCellPool);
         context->stateCellPool = CPID_NULL;
       }
 #endif
@@ -1267,7 +1267,7 @@ RecoveryStatePool *recoveryMakeStatePool(unsigned int stateCount) {
 }
 
 void recoveryRemoveStatePool(RecoveryStatePool *statePool) {
-  removeStatePool(statePool->cellPool);
+  removeRecoveryStatePool(statePool->cellPool);
   storageRelease(statePool, sizeof(RecoveryStatePool));
 }
 
@@ -1466,7 +1466,7 @@ int recoveryRemoveRouter() {
 
 #ifdef RCVR_CPOOL_STATES
   if (!(context->flags & RCVR_ROUTER_FLAG_USER_STATE_POOL)) {
-    removeStatePool(context->stateCellPool);
+    removeRecoveryStatePool(context->stateCellPool);
     context->stateCellPool = CPID_NULL;
   }
 #endif /* RCVR_CPOOL_STATES */
