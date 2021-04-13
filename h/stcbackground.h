ee4ca26b0f28cedc0804353e26c006a17c1d3e71
Nakul Manchanda <nakul.manchanda@ibm.com>
2021-04-13 13:20:04 -0400

    refactor on review
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- h/stcbackground.h
+++ h/stcbackground.h
@@ -16,30 +16,25 @@
 #define STC_BG_CB_LABEL_LEN 30
 
 struct STCIntervalCallbackData_tag;
-typedef int (*STCIntervalCallback)(void* server, 
-                                    STCBase* stcBase, 
+
+typedef int (*STCIntervalCallback)(STCBase* stcBase, 
                                     STCModule* module, 
                                     struct STCIntervalCallbackData_tag* callbackData, 
                                     void* userData
                                    );
-
 typedef struct STCIntervalCallbackData_tag {
   int id;
   char callbackLabel[STC_BG_CB_LABEL_LEN];
   STCIntervalCallback callback;
   void* userData;
-  int timeInterval;
+  int intervalSeconds;
   int countInterval;
 } STCIntervalCallbackData; 
 
-typedef struct STCCallbackList_tag {
-  void* server;
-  void* callbackList;
-} STCCallbackList;
 
-STCModule* stcInitBackgroundModule(void* server, STCBase *base);
-int stcAddIntervalCallback(STCModule *module, STCIntervalCallback callback, char* callbackLabel, int timeInterval, void* userData);
-int stcModifyInterval(STCIntervalCallbackData* callback, int newInterval);
+STCModule* stcInitBackgroundModule(STCBase *base);
+int stcAddIntervalCallback(STCModule *module, STCIntervalCallback callback, const char* callbackLabel, int intervalSeconds, void* userData);
+int stcModifyInterval(STCModule *module, const char* callbackLabel, int newIntervalSeconds);
 
 #endif 
 /*
