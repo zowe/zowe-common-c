f7cf5012e11fe1bf9f2e3f8e73a8fc71b6c59b42
Nakul Manchanda <nakul.manchanda@ibm.com>
2021-04-20 23:02:34 -0400

    added license
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- c/stcbackground.c
+++ c/stcbackground.c
@@ -1,3 +1,12 @@
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which 
+  accompanies this distribution, and is available at 
+  https://www.eclipse.org/legal/epl-v20.html
+  SPDX-License-Identifier: EPL-2.0
+  Copyright Contributors to the Zowe Project.
+*/
+
 #include <time.h>
 #include "logging.h"
 #include "stcbackground.h"
@@ -140,3 +149,12 @@ int stcModifyInterval(STCModule *module, const char* callbackLabel, int newInter
 
   return -1;
 }
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which 
+  accompanies this distribution, and is available at 
+  https://www.eclipse.org/legal/epl-v20.html
+  SPDX-License-Identifier: EPL-2.0
+  Copyright Contributors to the Zowe Project.
+*/
