07876b4ada1e111cc0f1edd9072df58b18dd3de5
James Struga <xhejmsstruga@gmail.com>
2022-06-09 23:17:11 -0400

    test
    Signed-off-by: James Struga <xhejmsstruga@gmail.com>

--- tests/test.bat
+++ tests/test.bat
@@ -2,6 +2,12 @@
 
 echo "********************************************************************************"
 echo "Running Test"
+echo ".."
+ls ..
+echo "../.."
+ls ../../deps
+echo "../../deps"
+ls ../../deps
 
 echo "Running convtest.c"
 clang -I../h -I../platform/windows -D_CRT_SECURE_NO_WARNINGS -Dstrdup=_strdup -DYAML_DECLARE_STATIC=1 -Wdeprecated-declarations --rtlib=compiler-rt -o convtest.exe convtest.c ../c/charsets.c ../platform/windows/winfile.c ../c/timeutls.c ../c/utils.c ../c/alloc.c
