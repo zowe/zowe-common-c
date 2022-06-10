c7f7a6e6d7e002df3fad7818cca75286bb6da137
James Struga <xhejmsstruga@gmail.com>
2022-06-09 23:34:37 -0400

    add test
    Signed-off-by: James Struga <xhejmsstruga@gmail.com>

--- tests/test.bat
+++ tests/test.bat
@@ -1,13 +1,21 @@
 @echo off
 
 echo "********************************************************************************"
-echo "Running Test"
-echo ".."
-ls ..
-echo "../.."
-ls ../../deps
-echo "../../deps"
-ls ../../deps
+
+set LIBYAML=../../deps/configmgr/libyaml
+set LIBYAML_LOCATION=git@github.com:yaml/libyaml.git
+set LIBYAML_BRANCH=0.2.5
+
+set DEPS="QUICKJS LIBYAML"
+
+LIBYAML="$../../deps/configmgr/libyaml"
+LIBYAML_LOCATION="git@github.com:yaml/libyaml.git"
+LIBYAML_BRANCH="0.2.5"
+
+git clone --branch 0.2.5 git@github.com:yaml/libyaml.git ../../deps.configmgr/libyaml
 
 echo "Running convtest.c"
 clang -I../h -I../platform/windows -D_CRT_SECURE_NO_WARNINGS -Dstrdup=_strdup -DYAML_DECLARE_STATIC=1 -Wdeprecated-declarations --rtlib=compiler-rt -o convtest.exe convtest.c ../c/charsets.c ../platform/windows/winfile.c ../c/timeutls.c ../c/utils.c ../c/alloc.c
+
+echo "Running charsetguesser.c"
+clang -I%LIBYAML%/include -I./src -I../h -I ../platform/windows -Dstrdup=_strdup -D_CRT_SECURE_NO_WARNINGS -o charsetguesser.exe charsetguesser.c ../c/xlate.c ../c/charsets.c ../c/winskt.c ../c/logging.c ../c/collections.c ../c/timeutls.c ../c/utils.c ../c/alloc.c 
