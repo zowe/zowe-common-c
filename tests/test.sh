1af30faa80fb3b36f69a31646bf896a9ef42fad4
James Struga <xhejmsstruga@gmail.com>
2022-06-09 22:07:14 -0400

    add more case
    Signed-off-by: James Struga <xhejmsstruga@gmail.com>

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,4 @@
 
-
 WORKING_DIR=$(dirname "$0")
 
 echo "********************************************************************************"
@@ -30,7 +29,7 @@ for dep in ${DEPS}; do
 done
 IFS=$OLDIFS
 
-
+echo "Running charsetguesser.c"
 clang \
   -I "${LIBYAML}/include" \
   -I./src \
@@ -48,3 +47,25 @@ clang \
   ../c/timeutls.c \
   ../c/utils.c \
   ../c/alloc.c 
+
+echo "Running convtest.c"
+clang \
+  -I../h \
+  -I../platform/windows \
+  -D_CRT_SECURE_NO_WARNINGS \
+  -Dstrdup=_strdup \
+  -DYAML_DECLARE_STATIC=1 \
+  -Wdeprecated-declarations \
+  --rtlib=compiler-rt \
+  -o convtest.exe \
+  convtest.c \
+  ../c/charsets.c \
+  ../platform/windows/winfile.c \
+  ../c/timeutls.c \
+  ../c/utils.c \
+  ../c/alloc.c
+  
+  
+  
+  
+  
