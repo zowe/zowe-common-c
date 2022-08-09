072ca175bf795991ff72342e770647fe42d7ff56
Irek Fakhrutdinov <ifakhrutdinov@rocketsoftware.com>
2022-08-09 18:46:16 +0200

    More robust look-up routine handling
    Signed-off-by: Irek Fakhrutdinov <ifakhrutdinov@rocketsoftware.com>

--- h/crossmemory.h
+++ h/crossmemory.h
@@ -373,6 +373,37 @@ typedef struct CrossMemoryServerStatus_tag {
   char descriptionNullTerm[64];
 } CrossMemoryServerStatus;
 
+typedef struct CMSLookupRoutineAnchor_tag {
+
+#define CMS_LOOKUP_ANCHOR_EYECATCHER   "ZWESISSV"
+#define CMS_LOOKUP_ANCHOR_VERSION      1
+#define CMS_LOOKUP_ANCHOR_KEY          CROSS_MEMORY_SERVER_KEY
+#define CMS_LOOKUP_ANCHOR_SUBPOOL      CROSS_MEMORY_SERVER_SUBPOOL
+#define CMS_LOOKUP_ANCHOR_HEADER_SIZE  0x28
+
+#define CMS_LOOKUP_ANCHOR_ROUTINE_VERSION 1
+
+  char eyecatcher[8];
+  uint8_t version;
+  uint8_t key;
+  uint8_t subpool;
+  char reserved1[1];
+  uint16_t size;
+  char reserved2[2];
+  /* Offset 0x10 */
+  uint64_t creationTime;
+  char jobName[8];
+  /* Offset 0x20 */
+  uint16_t asid;
+  char reserved22[4];
+
+  /* Offset 0x26 */
+  uint16_t routineVersion;
+  /* Offset 0x28 */
+  char routineBody[1024];
+
+} CMSLookupRoutineAnchor;
+
 ZOWE_PRAGMA_PACK_RESET
 
 #define LOG_COMP_ID_CMS       0x008F0001000C0001LLU
@@ -958,6 +989,42 @@ CrossMemoryServerName cmsMakeServerName(const char *nameNullTerm);
 #endif
 #define CMS_LOG_DUB_ERROR_MSG_TEXT              "Bad dub status %d (%d,0x%04X), verify that the started task user has an OMVS segment"
 #define CMS_LOG_DUB_ERROR_MSG                   CMS_LOG_DUB_ERROR_MSG_ID" "CMS_LOG_DUB_ERROR_MSG_TEXT
+
+#ifndef CMS_LOG_LOOKUP_ANC_CREATED_MSG_ID
+#define CMS_LOG_LOOKUP_ANC_CREATED_MSG_ID       CMS_MSG_PRFX"0251I"
+#endif
+#define CMS_LOG_LOOKUP_ANC_CREATED_MSG_TEXT     "Look-up routine anchor has been created at %p"
+#define CMS_LOG_LOOKUP_ANC_CREATED_MSG          CMS_LOG_LOOKUP_ANC_CREATED_MSG_ID" "CMS_LOG_LOOKUP_ANC_CREATED_MSG_TEXT
+
+#ifndef CMS_LOG_LOOKUP_ANC_REUSED_MSG_ID
+#define CMS_LOG_LOOKUP_ANC_REUSED_MSG_ID        CMS_MSG_PRFX"0252I"
+#endif
+#define CMS_LOG_LOOKUP_ANC_REUSED_MSG_TEXT      "Look-up routine anchor at % p has been reused"
+#define CMS_LOG_LOOKUP_ANC_REUSED_MSG           CMS_LOG_LOOKUP_ANC_REUSED_MSG_ID" "CMS_LOG_LOOKUP_ANC_REUSED_MSG_TEXT
+
+#ifndef CMS_LOG_LOOKUP_ANC_DELETED_MSG_ID
+#define CMS_LOG_LOOKUP_ANC_DELETED_MSG_ID       CMS_MSG_PRFX"0253I"
+#endif
+#define CMS_LOG_LOOKUP_ANC_DELETED_MSG_TEXT     "Look-up routine anchor at % p has been deleted"
+#define CMS_LOG_LOOKUP_ANC_DELETED_MSG          CMS_LOG_LOOKUP_ANC_DELETED_MSG_ID" "CMS_LOG_LOOKUP_ANC_DELETED_MSG_TEXT
+
+#ifndef CMS_LOG_LOOKUP_ANC_DISCARDED_MSG_ID
+#define CMS_LOG_LOOKUP_ANC_DISCARDED_MSG_ID     CMS_MSG_PRFX"0254W"
+#endif
+#define CMS_LOG_LOOKUP_ANC_DISCARDED_MSG_TEXT   "Look-up routine anchor at %p has been discarded due to %s:"
+#define CMS_LOG_LOOKUP_ANC_DISCARDED_MSG        CMS_LOG_LOOKUP_ANC_DISCARDED_MSG_ID" "CMS_LOG_LOOKUP_ANC_DISCARDED_MSG_TEXT
+
+#ifndef CMS_LOG_LOOKUP_ANC_ALLOC_ERROR_MSG_ID
+#define CMS_LOG_LOOKUP_ANC_ALLOC_ERROR_MSG_ID   CMS_MSG_PRFX"0255E"
+#endif
+#define CMS_LOG_LOOKUP_ANC_ALLOC_ERROR_MSG_TEXT "Look-up routine anchor has not been created"
+#define CMS_LOG_LOOKUP_ANC_ALLOC_ERROR_MSG      CMS_LOG_LOOKUP_ANC_ALLOC_ERROR_MSG_ID" "CMS_LOG_LOOKUP_ANC_ALLOC_ERROR_MSG_TEXT
+
+#ifndef CMS_LOG_LOOKUP_ANC_RACE_MSG_ID
+#define CMS_LOG_LOOKUP_ANC_RACE_MSG_ID          CMS_MSG_PRFX"0256W"
+#endif
+#define CMS_LOG_LOOKUP_ANC_RACE_MSG_TEXT        "New look-up routine anchor is not installed due to race condition with anchor %p"
+#define CMS_LOG_LOOKUP_ANC_RACE_MSG             CMS_LOG_LOOKUP_ANC_RACE_MSG_ID" "CMS_LOG_LOOKUP_ANC_RACE_MSG_TEXT
 
 #endif /* H_CROSSMEMORY_H_ */
 
