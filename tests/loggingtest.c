8ccaac5e9a79f203d4783ee0767e94c4c8f700f8
Jordan Filteau <jfilteau@rocketsoftware.com>
2024-04-30 13:53:30 -0500

    adding build script
    Signed-off-by: Jordan Filteau <jfilteau@rocketsoftware.com>

--- tests/loggingtest.c
+++ tests/loggingtest.c
@@ -9,7 +9,27 @@
 #include <inttypes.h>
 
 /*
- * Test that logging is functional with and without new helper macros.
+ BUILD
+        c89 -D_OPEN_THREADS -D_XOPEN_SOURCE=600 -DAPF_AUTHORIZED=0 -DNOIBMHTTP \
+        "-Wa,goff" "-Wc,langlvl(EXTC99),float(HEX),agg,list(),so(),search(),\
+        goff,xref,gonum,roconst,gonum,asm,asmlib('SYS1.MACLIB'),asmlib('CEE.SCEEMAC')" \
+        -I ../h \
+        -o loggingtest \
+        ./loggingtest.c \
+        ../c/utils.c \
+        ../c/logging.c \
+        ../c/alloc.c \
+        ../c/collections.c \
+        ../c/zos.c \
+        ../c/timeutls.c \
+        ../c/le.c \
+        ../c/scheduling.c \
+        ../c/recovery.c
+
+ */
+
+/*
+ * Test that logging is functional with and without new helper macros. Prove backwards compatibility.
  */
 void main() {
   LoggingContext *context = makeLoggingContext();
