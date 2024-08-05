ccbf89b1a3b77dad236c78231cd22cf34fe3b466
Jordan Filteau <jfilteau@rocketsoftware.com>
2024-08-05 16:18:02 -0500

    updating test and build
    Signed-off-by: Jordan Filteau <jfilteau@rocketsoftware.com>

--- tests/loggingtest.c
+++ tests/loggingtest.c
@@ -32,8 +32,8 @@
  * Test that logging is functional with and without new helper macros. Prove backwards compatibility.
  */
 void main() {
-  LoggingContext *context = NULL;
-  //LoggingContext *context = makeLoggingContext();
+  LoggingContext *context = makeLoggingContext();
+
   logConfigureStandardDestinations(context);
   logConfigureComponent(context, LOG_COMP_DATASERVICE, "DATASERVICE", LOG_DEST_PRINTF_STDOUT, ZOWE_LOG_INFO);
 
