91448c9adcc93e4a1cd939182a1fe57e81961443
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2025-09-08 13:20:19 +0200

    Minor updates
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- tests/configmgr/validate/index.sh
+++ tests/configmgr/validate/index.sh
@@ -13,8 +13,6 @@
 
 # Set anything for more tracing information
 TRACE=
-errors=0
-SCHEMAS="./schema/zowe-yaml-schema.json:./schema/server-common.json"
 
 if [ `uname` != "OS/390" ]; then
     echo "Error: this test must run on a z/OS system."
@@ -40,22 +38,28 @@ if [ ! -f "${configmgr_path}" ]; then
 fi
 
 printResults() {
-    expected="${1}"
-    real="${2}"
-    res="${3}"
-    if [ "${expected}" = "${real}" ]; then
-        echo "  OK, got and expected ${expected}\n"
+    schema="${1}"
+    configPath="${2}"
+    expectedRC="${3}"
+    realRC="${4}"
+    result="${5}"
+    echo ""
+    echo "*** configmgr -s(${schema}) -p(${configPath}) validate"
+    if [ "${expectedRC}" = "${realRC}" ]; then
+        echo "  OK, got and expected ${expectedRC}\n"
         if [ -n "${TRACE}" ]; then
-            echo "${res}" | sed "s/^/  /"
+            echo "${result}" | sed "s/^/  /"
         fi
     else
-        echo "  Error, got ${real}, expected ${expected}"
-        echo "${res}" | sed "s/^/  /"
+        echo "  Error, got ${realRC}, expected ${expectedRC}"
+        echo "${result}" | sed "s/^/  /"
         echo "\n"
         errors=`expr $errors + 1`
     fi
 }
 
+errors=0
+SCHEMAS="./schema/zowe-yaml-schema.json:./schema/server-common.json"
 
 # Make sure no abend for missing parameters
 # 4 => ZCFG_BAD_CONFIG_PATH
@@ -63,23 +67,23 @@ printResults() {
 
 IFS='|'
 while read expectedRC s p; do    
-    echo "\n*** configmgr -s(${s}) -p(${p}) validate"
     result=$(_CEE_RUNOPTS="XPLINK(ON)" "${configmgr_path}" -s "${s}" -p "${p}" validate 2>&1 > /dev/null)
     realRC=$?
-    printResults "${expectedRC}" "${realRC}" "${result}"
+    printResults "${s}" "${p}" "${expectedRC}" "${realRC}" "${result}"
 done <<EOF
 5||
 5||Config
 4|Schema|
+5|./schema/empty.json|./yaml/empty.yaml
+5|./schema/invalid.json|./yaml/valid.0.yaml
 EOF
 
 # Test the basic behavior
 for zoweYaml in ./yaml/*; do
     expectedRC=$(echo "${zoweYaml}" | awk -F. '{ print $3}')
-    echo "\n*** configmgr -s(${SCHEMAS}) -p(${zoweYaml}) validate"
     result=$(_CEE_RUNOPTS="XPLINK(ON)" "${configmgr_path}" -s "${SCHEMAS}" -p "FILE(${zoweYaml})" validate 2>&1 > /dev/null)
     realRC=$?
-    printResults "${expectedRC}" "${realRC}" "${result}"
+    printResults "${SCHEMAS}" "${zoweYaml}" "${expectedRC}" "${realRC}" "${result}"
 done
 
 exit $errors
