de1e05e0ac1eecfa127fe170eb5cb0b5f69626a8
1000TurquoisePogs <sgrady@rocketsoftware.com>
2026-04-08 11:13:57 -0400

    Back out c2124
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- platform/windows/quickjs_windows_compat.h
+++ platform/windows/quickjs_windows_compat.h
@@ -65,10 +65,6 @@ typedef int64_t ssize_t;
 #pragma warning(disable: 4018)  /* signed/unsigned mismatch in comparison */
 #pragma warning(disable: 4146)  /* unary minus on unsigned (neg hash tricks) */
 #pragma warning(disable: 4244)  /* narrowing integer/float conversion */
-/* C2124 is an *error* in MSVC for constant-expr divide-by-zero, but
-   quickjs.c deliberately uses  1.0/0.0  to produce IEEE 754 +Infinity.
-   Disable it for the duration of the QuickJS compile. */
-#pragma warning(disable: 2124)  /* divide or mod by zero (intentional inf) */
 
 #include <intrin.h>
 
