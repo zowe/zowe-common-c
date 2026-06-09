1fe03a3af8270e9383f3cdf10fb5db4a5ef55605
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2026-06-09 11:00:04 +0200

    Remove redundant memcpy
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- c/utils.c
+++ c/utils.c
@@ -1691,7 +1691,6 @@ char *stringListPrint(StringList *list, int start, int max, char *separator, cha
   /* printf("stringListPrint totalSize = %d listCount=%d buffer size %d, max=%d, slh=0x%x\n",
      list->totalSize,list->count,allocSize,max,list->slh); */
   out = SLHAlloc(list->slh, allocSize);
-  memcpy(out,"                        ",20);
   for (i=0; (i<start && elt); i++){
     elt = elt->next;
   }
@@ -1715,6 +1714,7 @@ char *stringListPrint(StringList *list, int start, int max, char *separator, cha
   }
   /* printf("string list formats to %d chars \n",pos); */
   out[pos] = 0;
+  printf("stringListPrint out '%s'\n", out);  // Delete me
   return out;
 }
 
