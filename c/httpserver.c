160bd78e925fb36630788eb2e5c67a4fb73424c5
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2026-06-09 08:41:12 +0200

    Rename variable 'now'
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- c/httpserver.c
+++ c/httpserver.c
@@ -1654,13 +1654,13 @@ static HttpServer *makeSecureHttpServerInner(STCBase *base, int port,
   server->config = (HttpServerConfig*)safeMalloc31(sizeof(HttpServerConfig),"HttpServerConfig");
   server->properties = htCreate(4001,stringHash,stringCompare,NULL,NULL);
   memset(server->config,0,sizeof(HttpServerConfig));
-  int64 now;
+  int64 sessionToken;
   int icsfReason = 0;
-  if (icsfGenerateRandomNumber(&now, sizeof(now), &icsfReason) != 0) {
-    now = getFineGrainedTime();
+  if (icsfGenerateRandomNumber(&sessionToken, sizeof(sessionToken), &icsfReason) != 0) {
+    sessionToken = getFineGrainedTime();
   }
-  server->config->sessionTokenKeySize = sizeof (now);
-  memcpy(&server->config->sessionTokenKey[0], &now, sizeof (now));
+  server->config->sessionTokenKeySize = sizeof (sessionToken);
+  memcpy(&server->config->sessionTokenKey[0], &sessionToken, sizeof (sessionToken));
   server->config->httpRequestHeapMaxBlocks = HTTP_REQUEST_HEAP_DEFAULT_BLOCKS;
 
   return server;
